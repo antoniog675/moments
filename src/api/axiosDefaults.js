de5b17321ed96778a9f63ada8a02c1c239d6e870
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-10-14 11:09:05 +0000

    added form to create account, error messages do not pop up when fields are empty, sumit button does not redirect to sign in page when submit button pressed

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://antonio-moments-drf-api.herokuapp.com/"
+axios.defaults.headers.post['Cotent-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
