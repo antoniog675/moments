8861ea8aab2653569efafb825689e8e4c1dac695
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-10-17 10:50:53 +0000

    signup form submitting properly now

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://antonio-moments-drf-api.herokuapp.com/"
-axios.defaults.headers.post['Cotent-Type'] = 'multipart/form-data'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
