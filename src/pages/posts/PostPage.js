a33766f06cefba697e2b245e6a9181ba529f7b0a
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-10-19 20:04:55 +0000

    added infinite scroll, all comments under a post show

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -12,6 +12,10 @@ import Post from "./Post";
 import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+import { Spinner } from "react-bootstrap";
 
 function PostPage() {
   const { id } = useParams();
@@ -56,12 +60,18 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map(comment => (
+            <InfiniteScroll children={comments.results.map((comment) => (
               <Comment key={comment.id} {...comment} 
               setPost={setPost} 
-              setComments={setComments} />
-            ))
-          ) : currentUser ? (
+              setComments={setComments}
+              />
+            ))}
+            dataLength={comments.results.length}
+            loader={<Asset spinner />}
+            hasMore={!!comments.next}
+            next={() => fetchMoreData(comments, setComments)}
+            />
+            ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ): (
             <span>No comments yet</span>
